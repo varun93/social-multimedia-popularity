b4d1925c62cb5ba21db3366d3fd2bd75218feb1d
Varun Hegde <varun@zenparent.in>
2017-10-31 17:19:24 +0530

    config file changed

--- TwitterDecay/config.py
+++ TwitterDecay/config.py
@@ -15,7 +15,7 @@ db = MySQLdb.connect(host = "127.0.0.1",user = "root",passwd = "root",db = "Twit
 
 news_channels_list=["TimesNow","ndtv","ibnlive","IndiaToday"]
 
-CONSUMER_KEY = "2WT1TSU4IlVNUgX9hUB2hkEwp"
-CONSUMER_SECRET  = "Bfh9WFZA4jUlGZj3DqgzhD8ecJ7zL78PDUYKQcM45WQofPoGUM"
-OAUTH_TOKEN = "767206872-duSzf95K69mSe0QvKXZRJtx0M9clovjeh23vrJPp"
-OAUTH_TOKEN_SECRET = "oGd3eGFaDXIgSJmpGUShahTDpDVXGEeklcn8utocmstUi"
+CONSUMER_KEY = ""
+CONSUMER_SECRET  = ""
+OAUTH_TOKEN = ""
+OAUTH_TOKEN_SECRET = ""
